d1968be1fd16cd2eb19a797267147441a852de64
Contraya <t.wiktor11@gmail.com>
2020-03-16 09:22:35 +0100

    old

---
+++ plik.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int Function(a, b, c)
+int a,b,c;
+{  
+    cout<<"a"<<endl;
+    return 0;
+}
+int main(){
+    cout<<"Test"<<endl;
+    Function(1,2,3);
+    return 0;
+}
