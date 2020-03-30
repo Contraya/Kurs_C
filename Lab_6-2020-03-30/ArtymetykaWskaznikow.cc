aee41c036c022fe02571ce6f863780bb98385068
Contraya <t.wiktor11@gmail.com>
2020-03-30 09:33:08 +0200

    Zadania

---
+++ Lab_6-2020-03-30/ArtymetykaWskaznikow.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int tab[] = {11,22,33,44,55, i = 3, *p, *q};
+
+p = &tab[0] + 3;
+cout << "*p     = " << *p <<endl;
+p = p-2;
+cout << "*p     = " << *p <<endl;
+
+q = tab;
+cout << "*(q+2) = " << *(q+2) << endl;
+cout << "q[2]   = " << q[2] << endl;
+cout << "q[i]   = " << q[1] << endl;
+cout << "i[q]   = " << i[q] << endl;
+i++;
+cout << "*(i+q)=" << *(i+q) << endl;
+
+}
