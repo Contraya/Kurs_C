0400ccc23cbb647ec8f1c3aa8d6f0bc59f4666e4
Contraya <58287031+Contraya@users.noreply.github.com>
2020-04-06 09:42:13 +0200

    Update zad2.cc

---
+++ Lab_7-2020-04-06/zad2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        char a = '0';
+        while (a != '!'){
+                cout << "Podaj znak !-kończy" << endl;
+                cin >> a;
+        }
+        return 0;
+
+}
