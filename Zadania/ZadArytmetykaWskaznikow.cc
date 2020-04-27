4183ede4794eb63100ebfa4fcd63b585fdf91150
Contraya <t.wiktor11@gmail.com>
2020-04-27 08:21:54 +0200

    Update ZadArytmetykaWskaznikow.cc

--- Zadania/ZadArytmetykaWskaznikow.cc
+++ Zadania/ZadArytmetykaWskaznikow.cc
@@ -2,20 +2,26 @@
 using namespace std;
 
 int main(){
-    int p = {1,2,3,4};
-
     int a[10] = {0,1,2,3,4,5,6,7,8,9};
     int *p = a;
     cout<<"p++: "<<p++<<endl;;
+    p++;
     cout<<"++p: "<<++p<<endl;
+    ++p;
     cout<<"++*p: "<<++*p<<endl;
+    cout << "*p = " << *p << endl;
     cout<<"++(*p): "<<++(*p)<<endl;
+    cout << "*p = " << *p << endl;
     cout<<"++*(p): "<<++*(p)<<endl;
     cout<<"*p++: "<<*p++<<endl;
+    cout << "*p = " << *p << endl;
     cout<<"(*p)++: "<<(*p)++<<endl;
+    cout << "*p = " << *p << endl;
     cout<<"*(p)++: "<<*(p)++<<endl;
     cout<<"*++p: "<<*++p<<endl;
+    cout << "*p = " << *p << endl;
     cout<<"*(++p): "<<*(++p)<<endl;
+    cout << "*p = " << *p << endl;
 
     return 0;
 }
