d8a9cd55a1b47d169b0fb0a3b3461f776389f71c
Contraya <58287031+Contraya@users.noreply.github.com>
2020-05-18 09:54:21 +0200

    Create ZadCandCPP.cc

---
+++ Zadania/ZadCandCPP.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // Wyjście
+    
+    // int w C
+    printf("%d\n", 20);
+ 
+    // int w C++
+    cout << 20 << endl;
+
+    // char w C
+    printf("%c\n", 'c');
+
+    // char w C++
+    cout << 'c' << endl;
+
+    // float w C
+    printf("%f\n", 20.05);
+
+    // float w C++
+    cout << double(20.05) << endl;
+
+    // string w C
+    char *string = "abcd test";
+    printf("%s\n", string);
+
+    // string w C++
+    cout << "daqkodkoaps" << endl;
+
+    // HEX w C
+    printf("%x\n", 20);
+
+    // HEX w C++
+    cout << hex << 20 << endl;
+}
